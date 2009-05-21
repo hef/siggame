732e28bbf2a37c6a4a237609ae39af3ca79f600a
Mike Hutcheson <mikehutch06@gmail.com>
2009-05-21 10:55:55 -0600

    added error check for Vista users added code for setting Python PATH environment variable
    [git-p4: depot-paths = "//depot/": change = 21]

--- utils/Source/SIGGameRBToolsInstall/resources/setenvs.py
+++ utils/Source/SIGGameRBToolsInstall/resources/setenvs.py
@@ -1,11 +1,35 @@
-import subprocess
+import subprocess, sys
+from easygui import msgbox
+
+# Test if user is running Vista
+p = subprocess.Popen("setenv.exe -m DUMMY_ENV_VAR TEMP", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out = p.communicate()
+
+# User is running Vista
+if p.returncode == 2:
+	msgbox(msg="Running Vista, access denied, please run setenvs.py in administrator mode.", title="Error", ok_button="Ok")
+	sys.exit()
+
+# User not using Vista, Delete the dummy environment variable and continue
+p = subprocess.Popen("setenv.exe -m DUMMY_ENV_VAR -delete", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
 
 #Get the PATH Environment variable
 p = subprocess.Popen("setenv.exe -m path", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out = p.communicate()
-
-#Set Perl PATH Environment Variable
+	
+#Set Msys PATH Environment Variable
 if out[0].lower().find("c:\\msys\\1.0\\bin") < 0:
 	print "Appending \";C:\\msys\\1.0\\bin\" to the PATH environment variable..."
 	cmd = "setenv.exe -m path \"" + out[0][:-2] + ";C:\\msys\\1.0\\bin\""				# [:-2] is to get rid of the \r\n at the end
 	subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+p = subprocess.Popen("setenv.exe -m path", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+out = p.communicate()
+
+#Set Python PATH Environment Variable	
+if out[0].lower().find("c:\\python25") < 0:
+	print "Appending \";C:\\Python25\" to the PATH environment variable..."
+	cmd = 'setenv.exe -m path "' + out[0][:-2] + ';C:\\Python25"'
+	subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
