6c342f40c00250f94f83ab9e29d90608ab2ba9ea
Sam Flores <sflore3@uic.edu>
2009-05-29 17:51:01 -0600

    Code Tutorial 1 Completed. Fixed "main.cpp" to follow coding standards. sln file reverted (didn't change).
    [git-p4: depot-paths = "//depot/": change = 53]

--- sandbox/sflores/project/SIGGame/main.cpp
+++ sandbox/sflores/project/SIGGame/main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "Utilities.h"
 
 int main()
 {
+	Utilities utils1;						// Instance of Utilities in Stack
+	Utilities* utils2 = new Utilities;	// Instance of Utilities in Heap
 	return 0;
 }
