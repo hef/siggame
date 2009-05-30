4b212ee27d65b5f1d445f9371dc98155e5351df0
Greg Jarzab <greg.jarzab@gmail.com>
2009-05-29 18:58:44 -0600

    Completed Code Tutorial #1 - Disregard (Issues with Vista)
    [git-p4: depot-paths = "//depot/": change = 55]

--- sandbox/gjarzab/project/SIGGame/main.cpp
+++ sandbox/gjarzab/project/SIGGame/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "Utilities.h"
 
 int main()
 {
+	Utilities	utilitiesOnStack;					//Utilities declared on the stack
+	Utilities*	pUtilitiesObject = new Utilities();	//Utilities on the heap
+
 	return 0;
 }
