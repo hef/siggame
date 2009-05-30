77bcf1ff8d8988d7bbaaf3daa4ff7556b4da713a
Pete Kowalski <pkowal3@uic.edu>
2009-05-30 11:17:01 -0600

    Rollback //depot/development/SIGGame/main.cpp to revision 1
    [git-p4: depot-paths = "//depot/": change = 73]

--- development/SIGGame/main.cpp
+++ development/SIGGame/main.cpp
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-// to do:
-// ask sakkos his thoughts of "void main()" vs "int main()"
 int main()
 {
-	Utilities  a_OnStack;
-	Utilities* a_OnHeap = new Utitilies();
 	return 0;
 }
