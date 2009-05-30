b6ea2ed8306700d05cb1fa4c500e155ef5c6f6f5
Pete Kowalski <pkowal3@uic.edu>
2009-05-30 11:15:34 -0600

    Tutorial 1
    [git-p4: depot-paths = "//depot/": change = 71]

--- development/SIGGame/main.cpp
+++ development/SIGGame/main.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+// to do:
+// ask sakkos his thoughts of "void main()" vs "int main()"
 int main()
 {
+	Utilities  a_OnStack;
+	Utilities* a_OnHeap = new Utitilies();
 	return 0;
 }
