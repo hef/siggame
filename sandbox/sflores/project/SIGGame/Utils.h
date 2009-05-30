8d321217cc36565fe59b74cb2963aea53419a3df
Sam Flores <sflore3@uic.edu>
2009-05-30 07:56:37 -0600

    Code Tutorial #2 Rename/Move: From: //depot/sandbox/sflores/project/SIGGame/Utilities.h To: //depot/sandbox/sflores/project/SIGGame/Utils.h
    [git-p4: depot-paths = "//depot/": change = 61]

---
+++ sandbox/sflores/project/SIGGame/Utils.h
@@ -0,0 +1,11 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+class Utils
+{
+	public:
+		Utils() { }		// Default Constructor
+		~Utils() { }	// Destructor
+};
+
+#endif
--- sandbox/sflores/project/SIGGame/main.cpp
+++ sandbox/sflores/project/SIGGame/main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include "Utilities.h"
+#include "Utils.h"
 
 int main()
 {
-	Utilities utils1;						// Instance of Utilities in Stack
-	Utilities* utils2 = new Utilities;	// Instance of Utilities in Heap
+	Utils utils1;				// Instance of Utilities in Stack
+	Utils* utils2 = new Utils;	// Instance of Utilities in Heap
 	return 0;
 }
