db1bbc586fa671de2e9ebe3050008ca07f8bbde2
Sam Flores <sflore3@uic.edu>
2009-05-30 20:10:26 -0600

    Code Tutorial #3: Added a sumUp method to the Utils class. Tested in "main.cpp".
    [git-p4: depot-paths = "//depot/": change = 79]

--- sandbox/sflores/project/SIGGame/Utils.h
+++ sandbox/sflores/project/SIGGame/Utils.h
@@ -6,6 +6,19 @@ class Utils
 	public:
 		Utils() { }		// Default Constructor
 		~Utils() { }	// Destructor
+		
+		// Calculates sum from 1 to nTop
+		int sumUp( int nTop )
+		{
+			int nResult = 0;
+			// Code enters for loop if nTop >= 1,
+			// otherwise nResult = 0 is returned
+			for( int i = 1; i <= nTop; ++i )
+			{
+				nResult += i;
+			}
+			return nResult;
+		}
 };
 
 #endif
--- sandbox/sflores/project/SIGGame/main.cpp
+++ sandbox/sflores/project/SIGGame/main.cpp
@@ -1,9 +1,22 @@
 #include <stdio.h>
+#include <iostream>
 #include "Utils.h"
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main()
 {
 	Utils utils1;				// Instance of Utilities in Stack
 	Utils* utils2 = new Utils;	// Instance of Utilities in Heap
+	
+	// Test Utils' sumUp method
+	int n;
+	cout << "Enter n to calculate the sum from 1 to n: ";
+	// Works with any integer value >= 1. Otherwise, returns zero.
+	cin >> n;
+	cout << "The sum from 1 to " << n << " is: " << utils1.sumUp( n ) << endl;
+	cin >> n;
+
 	return 0;
 }
