338a37d0057adb0d451bd7d1108353f1284add58
Andrew Suh <giggedy.giggedy.giggedy@gmail.com>
2009-06-01 15:52:55 -0600

    Tutorial 1: Created Utilities.h in sandbox Added instance of Utilities onto stack and heap Moved * closer to type (Utilities)
    [git-p4: depot-paths = "//depot/": change = 100]

---
+++ sandbox/asuh/project/SIGGame/Utilities.h
@@ -0,0 +1,23 @@
+//Andrew Suh
+//May 28, 2009
+
+// #ifndef is a header guard that prevents errors from including 
+// the same header multiple times
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+class Utilities
+{
+public:
+	Utilities()
+	{
+		//Constructor
+	}
+
+	~Utilities()
+	{
+		//Destructor
+	}
+};
+
+#endif //UTILITIES_H
--- sandbox/asuh/project/SIGGame/main.cpp
+++ sandbox/asuh/project/SIGGame/main.cpp
@@ -1,6 +1,12 @@
+//Andrew Suh
+//May 28, 2009
+
 #include <stdio.h>
+#include "Utilities.h"
 
 int main()
 {
+	Utilities uForStack;
+	Utilities* uForHeap = new Utilities;	//http://www.java2s.com/Tutorial/Cpp/0180__Class/Creatingobjectsontheheapusingnew.htm
 	return 0;
 }
