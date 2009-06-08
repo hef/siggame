b9c54398e6a666bb861f0e046eabf560d2da7f8b
idelgado <idelga2@uic.edu>
2009-06-08 14:54:18 -0600

    Tutorial 1 made a utilities header class.
    [git-p4: depot-paths = "//depot/": change = 163]

--- sandbox/idelgado/project/SIGGame/main.cpp
+++ sandbox/idelgado/project/SIGGame/main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "Utilities.h"
 
+void createUtilities();
 int main()
 {
+	Utilities test1;
+	
+createUtilities();
+
 	return 0;
+}
+
+void createUtilities()
+{
+Utilities test2;
 }
