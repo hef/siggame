a0ebaa9a5f2097995005625e51706f37a56bf345
Hef <hef@pbrfrat.com>
2009-06-09 01:22:00 -0600

    wasn't supposed to commit these yet.
    [git-p4: depot-paths = "//depot/": change = 170]

--- sandbox/hef/tutorial3/Utils.cpp
+++
@@ -1,5 +0,0 @@
-#include "Utils.h"
-int Utils::sumUp(const int n)
-{
-	return ((n*(n+1))/2);
-}
--- sandbox/hef/tutorial3/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Utils.h"
-int main( int argc, char **argv )
-{
-  Utils* pUtil = new Utils;
-  Utils  Util;
-  std::cout << pUtil->sumUp(50) << std::endl;
-  delete pUtil;
-}
