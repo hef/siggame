4ff6f435355aa93e083225eee09f9d867f27ac0c
Hef <hef@pbrfrat.com>
2009-06-09 11:04:35 -0600

    ensuring failure on negative numbers
    [git-p4: depot-paths = "//depot/": change = 173]

--- sandbox/hef/tutorial3/Utils.h
+++ sandbox/hef/tutorial3/Utils.h
@@ -6,6 +6,9 @@ private:
 public:
   Utils(){}
   ~Utils(){}
-  int sumUp( const int n );
+  int sumUp( const unsigned int n );
+  //causes sumUp to fail on non unsigned int parameters
+  template <class T>
+  int sumUp( T n );
 };
 #endif /* Utils_H */
