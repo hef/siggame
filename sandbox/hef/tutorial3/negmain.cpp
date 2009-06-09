34497349a59778a26149a557e024e998faa762ae
Hef <hef@pbrfrat.com>
2009-06-09 11:04:39 -0600

    indicated an intentional failure
    [git-p4: depot-paths = "//depot/": change = 174]

--- sandbox/hef/tutorial3/negmain.cpp
+++ sandbox/hef/tutorial3/negmain.cpp
@@ -4,6 +4,7 @@ int main( int argc, char **argv )
 {
   Utils* pUtil = new Utils;
   Utils  Util;
+  //this line should cause a comiler error
   std::cout << pUtil->sumUp( -50 ) << std::endl;
   delete pUtil;
 }
