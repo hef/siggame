986bc836b2ce4d5cc112ed9057e4104528078395
Hef <hef@pbrfrat.com>
2009-06-29 12:20:45 -0600

    preparing to submit, combining 24 commits
    working on headers for actor, model, surface
    
    cleanup
    
    renamed getP to getArray
    
    working on surfaces
    
    added access motheods to surface
    
    added functions I need
    
    still working on it
    
    updated Vector3f from reviewboard
    
    working with surface still
    
    removed useless comment
    
    updated Surface and Vector3f classes
    
    removed useless comment
    
    got rid of unneccesary union
    
    updated model class
    
    cleanups
    
    updated Actor class
    
    holy moly I think the new renderer is working
    
    making less things pointy
    
    deleteing unused classes
    
    now drawing 2 actors ont he screen
    
    fixed copy constuctor bug
    
    expirimenting with changes to Vector3f and surface
    
    made Surface behave more like the sakkos wants
    
    cleaned up comments
    
    [git-p4: depot-paths = "//depot/": change = 276]

---
+++ development/SIGGame/Model.h
@@ -0,0 +1,21 @@
+#ifndef MODEL_H
+#define MODEL_H
+#include <vector>
+#include "Surface.h"
+#include "Vector3f.h"
+/**
+  A model has a std::vector of surfaces
+  All models are composed of triangles
+*/
+class Model
+{
+	private:
+		std::vector<Surface> surfaces;
+	public:
+		///loads a sample model
+		Model();
+		Model(const Model& model);
+		~Model();
+		const std::vector<Surface>& getSurfaces() const;
+};
+#endif
---
+++ development/SIGGame/Surface.cpp
@@ -0,0 +1,21 @@
+#include "Surface.h"
+Surface::Surface(const Vector3f A, const Vector3f B, const Vector3f C)
+{
+	corners[0]=A;
+	corners[1]=B;
+	corners[2]=C;
+}
+Surface::Surface(const Surface& surface)
+{
+	corners[0]=surface.corners[0];
+	corners[1]=surface.corners[1];
+	corners[2]=surface.corners[2];
+}
+Surface::~Surface()
+{
+}
+Vector3f& Surface::operator[](const int i)
+{
+	assert( i >= 0 && i < 3 && "Operator[] out of bounds assert");
+	return corners[i];
+}
