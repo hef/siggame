4606fc0d39eb74a7d1999368a5c8cd1856fea657
Mike Hutcheson <mikehutch06@gmail.com>
2009-07-06 16:39:37 -0600

    Added for merge tutorial
    [git-p4: depot-paths = "//depot/": change = 294]

---
+++ sandbox/hutch/project/SIGGame/MergeTest.cpp
@@ -0,0 +1,17 @@
+//Simple test for a merge
+
+// Some Stubs
+int add( int a, int b )
+{
+	return 0;
+}
+
+int subtract( int a, int b )
+{
+	return 0;
+}
+
+int multiply( int a, int b )
+{
+	return a * b;
+}
