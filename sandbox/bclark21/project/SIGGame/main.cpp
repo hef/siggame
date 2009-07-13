cfbd659f777c31f283c8e0d51151cd32e5f4a4e6
Bennett Clark <bclark21@uic.edu>
2009-07-13 16:48:55 -0600

    Tutorial 1 nearing completion
    [git-p4: depot-paths = "//depot/": change = 307]

--- sandbox/bclark21/project/SIGGame/main.cpp
+++ sandbox/bclark21/project/SIGGame/main.cpp
@@ -1,13 +1,55 @@
-#ifndef UNIQUE
-#define UNIQUE
+/*****************************************************************************/
+//header guards to prevent multiple compilation of definitions
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+// Declaration of class Utilities
+/* filename: Utilities.h			*/
+
 #include <stdio.h>
+//using namespace std;
+
+class Utilities{
+	private:
+		int digit;
+	public:
+
+		//Default constructor:
+		Utilities(){
+			digit = 0;
+		}
+
+		//Destructor:
+		~Utilities(){
+
+		}
+};
+#endif /* UTILITIES_H */  //end of compiler directive ndef
+
+
+/*****************************************************************************/
+
+//  filename:  Utilities.cpp
+
+#include <stdio.h>
+#include <iostream>
+#include <stack>
+//#include "Utilities.h"
+using namespace std;
 
 int main()
 {
 	int ready;
+	//Declaration of Objects
+	Utilities *useful,
+			  semiuseful;
+	
 	cout<<"Hello";
 	cin>>ready;
+
+	useful = new Utilities::Utilities();
+
+	
 	return 0;
 }
 
-#endif /* UNIQUE */
