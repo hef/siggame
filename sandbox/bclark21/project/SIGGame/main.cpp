2ef301c49c74a5e900bd6f76b625ef1a22f4b143
Bennett Clark <bclark21@uic.edu>
2009-07-14 18:18:13 -0600

    with separate header file for Utilities
    [git-p4: depot-paths = "//depot/": change = 313]

--- sandbox/bclark21/project/SIGGame/main.cpp
+++ sandbox/bclark21/project/SIGGame/main.cpp
@@ -1,67 +1,12 @@
-/*****************************************************************************/
-//header guards to prevent multiple compilation of definitions
-#ifndef UTILITIES_H
-#define UTILITIES_H
-
-// Declaration of class Utilities
-/* filename: Utilities.h			*/
-
-#include <stdio.h>
-//using namespace std;		//only if the class is declared in a separate file
-
-class Utilities{
-	
-	//private members
-	private:
-		//private data members
-		int digit;
-	
-	//public members
-	public:
-		//public data
-		int digitPub;
-
-		//Default constructor:
-		Utilities(){
-			digit = 8;
-			digitPub = 9;
-		}
-
-		//parameterized constructor:
-		Utilities(int initDigit){
-			digit = initDigit;
-			digitPub = 9;
-		}
-
-		//accessor methods
-		int Utilities::getDigit(){
-			return digit;
-		}
-
-		int Utilities::getDigitPub(){
-			return digitPub;
-		}
-
-		//mutator method
-		void Utilities::set(int newDigit){
-			digit = newDigit;
-		}
-
-		//Destructor:
-		~Utilities(){
-
-		}
-};
-#endif /* UTILITIES_H */  //end of compiler directive ndef
 
 
 /*****************************************************************************/
 
 //  filename:  Utilities.cpp	//declares an executable source code
 
-#include <stdio.h>
-#include <iostream>
-//#include "Utilities.h"  //used only when the class declaration is in 
+//#include <stdio.h>
+#include <iostream>		//required for cin, cout, endl
+#include "Utilities.h"  //used only when the class declaration is in 
 //a separate headerfile
 
 using namespace std;
