8e70caedf0d77993fb4769f1633161f183a69ad0
Hef <hef@pbrfrat.com>
2009-07-22 00:30:08 -0600

    player actor now moves
    code cleanup
    
    [git-p4: depot-paths = "//depot/": change = 358]

--- development/SIGGame/PlayerActor.cpp
+++ development/SIGGame/PlayerActor.cpp
@@ -1,5 +1,4 @@
 #include "PlayerActor.h"
-
 PlayerActor::PlayerActor( Model* model, const Vector3f position, const Vector3f rotation )
 	: Actor( model, position, rotation )
 {
@@ -22,30 +21,35 @@ void PlayerActor::tick( const double dt )
 {
 	Input* input = Input::Instance();
 
+	//TODO make 100.0f configureable
 	if( input->leftArrowDown() ){
 		rotation[2] += 100.0f * (float)dt;
 	}
 	else if( input->rightArrowDown() ){
 		rotation[2] -= 100.0f * (float)dt;
 	}
-
-	/* TODO read up on math to fill this in
-	   the concept is:
-	   take the rotation[2] x component,
-	   add it to dPosition[0]
-	   take the rotation[2] y component, 
-	   add it to dPosition[1]
-	   then apply normal vector movement(pos+=dpos*dt)
-	   
-	   remember, sin/cos methods suck cycles fast, there is probably a 
-	   vector/matrix method that is faster and better
-
-	if(input->upDown()){
-		rotation[0] += dRotation[0];
+	if(input->upArrowDown())
+	{	
+		const float PI = 3.14159265;
+		//TODO 10.0f is a magnitude of speed change. make it configureable
+		//add the x vector componet of direction to x component of velocity
+		dPosition[0] += 10.0f * cos( rotation[2] * PI/180.0f ) * (float)dt;
+		//add the y vector componet of rotation to y component of velocity
+		dPosition[1] += 10.0f * sin( rotation[2] * PI/180.0f ) * (float)dt;
 	}
-	else if(input->downDown()){
-		rotation[0] -= dRotation[0];
+
+	//TODO The numbers 7 and 14 need to be adjusted to fit veiwport size
+	for ( int i=0; i<3 ; ++i )
+	{
+		position[ i ] += dPosition[ i ] * (float)dt;
+		while ( position[ i ] > 7 )
+		{
+			position[ i ] += -14;
+		}
+		while ( position[ i ] < -7 )
+		{
+			position[ i ] += 14;
+		}
 	}
-	*/
 
 }
