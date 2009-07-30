798c902e5252f8ea435c79b67fd02d2206ff5f5d
Kevin Green <kgreen20@uic.edu>
2009-07-29 22:14:38 -0600

    Tutorial 3
    [git-p4: depot-paths = "//depot/": change = 402]

--- sandbox/kgreen20/project/SIGGame/main.cpp
+++ sandbox/kgreen20/project/SIGGame/main.cpp
@@ -1,9 +1,26 @@
-#include <stdio.h>
-#include "Utilities.h"
+#include <iostream>
+#include <assert.h>
+#include "Utils.h"
+
+using std::cout;
 
 int main()
 {
-	Utilities* utilitiesHeap = new Utilities();
-	Utilities utilitiesStack;
+	Utils* utilitiesHeap = new Utils();
+	Utils utilitiesStack;
+
+	//Tutorial 3
+
+	int testVectors[] = {1,3,5,6,10};
+	int testExpected[] = {1,6,15,21,55};
+	int actual;
+
+	for(int i = 0; i < 5; i++)
+	{
+		assert( (utilitiesStack.sumUp(testVectors[i])) == (testExpected[i]));
+	}
+	cout << "sumUp appears to work...\n" ;
+
+	system( "PAUSE" ); 
 	return 0;
 }
