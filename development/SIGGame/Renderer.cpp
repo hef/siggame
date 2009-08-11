b82c4fa183b90d760d747a68d85449d4328fdca2
Hef <hef+storm@pbrfrat.com>
2009-08-11 01:15:12 -0500

    shading looks more correct now

--- development/SIGGame/Renderer.cpp
+++ development/SIGGame/Renderer.cpp
@@ -121,8 +121,8 @@ void Renderer::initGL( GLsizei width, GLsizei height )
 	glEnable( GL_DEPTH_TEST );
 	glDepthFunc( GL_LEQUAL );
 
-	glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST ); // nicest calculation possible
-	glEnable( GL_PERSPECTIVE_CORRECTION_HINT ); // enabled above
+	//glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST ); // nicest calculation possible
+	//glEnable( GL_PERSPECTIVE_CORRECTION_HINT ); // enabled above
 	
 	glEnable(GL_LIGHTING);
 	glEnable(GL_COLOR_MATERIAL);
@@ -130,9 +130,9 @@ void Renderer::initGL( GLsizei width, GLsizei height )
 
 	// Create light components
 	float ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
-	float diffuseLight[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	float specularLight[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	float position[] = { 0.0f, 0.0f, 0.0f, 1.0f };
+	float diffuseLight[] = { 1.0f, 0.3f, 0.0f, 1.0f };
+	float specularLight[] = { 0.0f, 0.0f, 0.8f, 1.0f };
+	float position[] = { 0.0f, 0.0f, -100.0f, 1.0f };
 	//
 	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
