082e9004b1fd6a338b4717b99420db45bdcfc65a
Hef <hef@hefs-macbook-pro-17.local>
2009-08-14 01:21:16 -0500

    documented a few classes

--- development/SIGGame/ModelSceneNode.h
+++ development/SIGGame/ModelSceneNode.h
@@ -6,10 +6,17 @@
 #include "Vertex.h"
 #include "Mesh.h"
 #include "Material.h"
+/**
+ * ModelSceneNode contains all the information neccesary to draw a model
+ * ModelSceneNode should be considered render code.  Its most important function
+ * is render().
+ * */
 class ModelSceneNode : public SceneNode
 {
 private:
+	///The big list of vertices that mesh's will refer to by index
 	std::vector<Vertex> vertex;
+	///Meshs.  each mesh has a material to decied how the vertices in the mesh will be drawn
 	std::vector<Mesh> mesh;
 public:
 	ModelSceneNode();
