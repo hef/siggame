3f0a60b1cd7c73a4a4591b05b69397564d0fee41
Hef <hef@hefs-macbook-pro-17.local>
2009-08-14 01:21:16 -0500

    documented a few classes

--- development/SIGGame/Mesh.h
+++ development/SIGGame/Mesh.h
@@ -3,12 +3,22 @@
 #include "Vector3f.h"
 #include <vector>
 #include "Material.h"
+/**
+ * A Mesh is a Material and a collection of vertices.
+ * A mesh really only makes sense if it is used in a modelSceneNode
+ */
 class Mesh
 {
 public:
 	Mesh();
 	Mesh( const Material& material, const std::vector< std::vector<int> >& vertexIndex );
+	/** the material that is used for all vertexs contained withing
+	 */
 	const Material& getMaterial() const;
+	/** 
+	 * index of the vertices contained in the parent modelSceneNode that
+	* contains this object
+	*/
 	const std::vector< std::vector<int> >& getVertexIndex() const;
 private:
 	Material material;
