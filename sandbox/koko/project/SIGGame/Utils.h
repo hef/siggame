107fda9b369b1cd4c825b2cf4a9e1ba438746e4a
Tony Smith <asmith39@iit.edu>
2009-08-16 08:23:45 -0600

    Tutorial 3
    [git-p4: depot-paths = "//depot/": change = 434]

--- sandbox/koko/project/SIGGame/Utils.h
+++ sandbox/koko/project/SIGGame/Utils.h
@@ -5,9 +5,17 @@
 class Utils
 {
 	public:
-		Utils(){}
+		Utils();
 
-		~Utils(){}
+		~Utils();
+
+	unsigned int sumUp( unsigned int n );
+
+	private:
+		int sumUp( int n );
+		int sumUp( float n );
+		int sumUp( double n );
+		int sumUp( long n );
 };
 
 #endif //UTILS_H
