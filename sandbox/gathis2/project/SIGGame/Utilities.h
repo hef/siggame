46fe84a6ef0c6b1adbacdad75843f92ea9ee04c7
Gurumurthy Athisenbagam <gathis2@uic.edu>
2009-09-08 23:39:06 -0600

    Tutorial 1
    [git-p4: depot-paths = "//depot/": change = 444]

---
+++ sandbox/gathis2/project/SIGGame/Utilities.h
@@ -0,0 +1,20 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <iostream>
+
+using namespace std;
+
+class Utilities //Sample Class for the C++ Tutorial 
+   {
+         public: 
+         Utilities() //Constructor for the C++ tutorial 
+         { 
+             cout<< "Constructor" << endl;
+         }
+       ~Utilities() //destructor for the C++ Tutorial 
+       { cout<< "Constructor" << endl; } 
+      
+}; 
+
+#endif
