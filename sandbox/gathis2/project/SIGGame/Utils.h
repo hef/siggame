696d5e8cced215e3b60d283689f82967f7dbdced
Gurumurthy Athisenbagam <gathis2@uic.edu>
2009-09-09 01:42:46 -0600

    Tutorial 3 - review
    [git-p4: depot-paths = "//depot/": change = 449]

--- sandbox/gathis2/project/SIGGame/Utils.h
+++ sandbox/gathis2/project/SIGGame/Utils.h
@@ -1,28 +1,33 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <iostream>
-
-using namespace std;
-
 class Utils //Sample Class for the C++ Tutorial 
-   {
-         public: 
-         Utils() //Constructor for the C++ tutorial 
-         { 
-             cout<< "Constructor" << endl;
-         }
-       ~Utils() //destructor for the C++ Tutorial 
-       { cout<< "Constructor" << endl; } 
-
-	   int sumUp(int n)
-	   {
-		   int sum=0;
-		   for(int i=1;i<=n;i++)
-			   sum = sum+i;
-		   return sum;
-	   }
-      
+{
+	public: 
+		Utils(); //Constructor for the C++ tutorial 
+		~Utils(); //destructor for the C++ Tutorial 
+		int sumUp(int n);
 }; 
+
+class Dog
+{
+public:
+	float height;
+	float weight;
+	char* name;
+
+	float getHeight();
+	float getWeight();
+	char* getName();
+	void setHeight(float);
+	void setWeight(float);
+	void setName(char*);
+	Dog(const Dog& x);
+	const Dog& operator=(const Dog& );
+	Dog();
+	Dog(char* n);
+	~Dog();
+
+};
 
 #endif
--- sandbox/gathis2/project/SIGGame/main.cpp
+++ sandbox/gathis2/project/SIGGame/main.cpp
@@ -2,10 +2,31 @@
 #include "Utils.h"
 #include <conio.h>
 
+using namespace std;
+
 int main()           
 {
 	Utils U;
 	cout<<endl;
-	cout<<U.sumUp(5);
+	int sum =U.sumUp(5);
+	if(sum == 0)
+		cout<<"Input a positive number";
+	else
+		cout<<sum;
+
+	Dog d1("ruby");
+	cout<< endl;
+	cout<< d1.getName();
+
+	d1.setHeight(2.5);
+
+	Dog d2;
+	d2=d1;
+
+	cout<< endl;
+	cout<< d2.getName();
+	cout<< endl;
+	cout<< d2.getHeight();
+
 	getch();
 }
